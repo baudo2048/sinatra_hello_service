39e858f327668d97447bf11ec45d36c1c555fdc7
Pito Salas <pitosalas@gmail.com>
2022-03-10 14:39:25 -0500

    wip

--- service_app.rb
+++ service_app.rb
@@ -8,7 +8,7 @@ class ServiceApp < Sinatra::Base
   get "/api/sync/?" do
     content_type :json
     session[:count] = 0 if session[:count].nil?
-    { message: "Sync called #{session[:count]}"}.to_json
     session[:count] += 1
+    { message: "Sync called #{session[:count]}"}.to_json
   end
 end
