1ce2cb46b3dab269632245664aaa9df790c54659
Pito Salas <pitosalas@gmail.com>
2022-03-10 15:08:11 -0500

    wip

--- service_app.rb
+++ service_app.rb
@@ -2,25 +2,9 @@ require 'sinatra/base'
 require 'json'
 
 class ServiceApp < Sinatra::Base
-  def self.get_global key
-    @globals[key]
-  end
-
-  def self.set_global key, value
-    @globals[key] = value
-  end
-
-  def self.incr_global key
-    if @globals[key].nil
-      @globals[key] = 0
-    else
-      @globals[key] += 1
-    end
-    @globals[key]
-  end
-
   get "/api/sync/?" do
     content_type :json
-    { message: "Sync called #{incr_global(:counter)}"}.to_json
+    sleep 3
+    {message: "Sync api called"}.to_json
   end
 end
