3b087199f3e47c75708083343977cf1bfe888595
Pito Salas <pitosalas@gmail.com>
2022-03-10 08:56:39 -0500

    wip

--- service_app.rb
+++ service_app.rb
@@ -1,7 +1,13 @@
 require 'sinatra/base'
+require 'json'
+
 
 class ServiceApp < Sinatra::Base
+  before do
+    content_type :json
+  end
+
   get "/api/synch" do
-    puts "Sync Random Called!"
+    { message: "Sync Random Called!"}.to_json
   end
 end
