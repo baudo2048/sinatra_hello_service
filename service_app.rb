e8a0deb2087cd02727d0b33de2a8133037498dda
Pito Salas <pitosalas@gmail.com>
2022-03-10 15:09:33 -0500

    wip

--- service_app.rb
+++ service_app.rb
@@ -5,6 +5,6 @@ class ServiceApp < Sinatra::Base
   get "/api/sync/?" do
     content_type :json
     sleep 3
-    {message: "Sync api called"}.to_json
+    {message: "Sync api called: #{Time.now}"}.to_json
   end
 end
