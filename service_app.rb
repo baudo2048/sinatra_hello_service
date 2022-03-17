934135db4de5788f5ee396cdec6000d0c85049fb
Pito Salas <pitosalas@gmail.com>
2022-03-16 21:00:31 -0400

    wip

--- service_app.rb
+++ service_app.rb
@@ -3,7 +3,14 @@ require 'json'
 require 'logger'
 
 class ServiceApp < Sinatra::Base
+
+  before do
+    @logger = Logger.new(STDOUT)
+  end
+
+
   get "/api/sync/?" do
+    @logger.info "Hello Paper Trail"
     content_type :json
     sleep 3
     {message: "Sync api called: #{Time.now}"}.to_json
