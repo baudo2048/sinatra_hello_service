47b9446b20bc01a18bac68076cd19c43311b515a
Pito Salas <pitosalas@gmail.com>
2022-03-17 15:25:44 -0400

    wip

--- service_app.rb
+++ service_app.rb
@@ -38,7 +38,7 @@ class ServiceApp < Sinatra::Base
                         final_total: "#{final_total}"
                       })
     end
-    {sync_time: "#{Time.now}"}.to_json
+    {message: " #{Time.now}"}.to_json
   end
 
   def create_random_user(count)
