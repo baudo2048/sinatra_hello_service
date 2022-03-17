0a31779b48d6f45749399fd05a6fbbfe6f346da7
Pito Salas <pitosalas@gmail.com>
2022-03-17 14:15:07 -0400

    wip

--- service_app.rb
+++ service_app.rb
@@ -4,12 +4,20 @@ require 'logger'
 require 'faker'
 require 'active_record'
 require 'sinatra/activerecord'
+require 'pusher'
 require_relative 'models/user'
 
 class ServiceApp < Sinatra::Base
   before do
     @logger = Logger.new($stdout)
     @logger.info "Hello Paper Trail: this is servapp"
+    @pusher = Pusher::Client.new(
+      app_id: '1363367',
+      key: 'dd9f23cab2c8652cbd08',
+      secret: 'ebea9a2c32552c6fb48b',
+      cluster: 'us2',
+      encrypted: true
+    )
   end
 
   get "/api/user/add/sync/?" do
@@ -21,7 +29,12 @@ class ServiceApp < Sinatra::Base
 
   get "/api/user/add/async/?" do
     content_type :json
-    Thread.new { create_random_user(params[:user_count].to_i) }
+    Thread.new do
+      create_random_user(params[:user_count].to_i)
+      pusher.trigger('my-channel', 'my-event', {
+                       message: 'hello world'
+                     })
+    end
     {message: "Sync api called: #{Time.now}"}.to_json
   end
 
