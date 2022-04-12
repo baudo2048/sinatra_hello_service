c09c2111f871d85fb15f32fe4a72412e19a29282
Pito Salas <pitosalas@gmail.com>
2022-04-12 10:18:49 -0400

    wip

--- lib/work_queue.rb
+++ lib/work_queue.rb
@@ -2,26 +2,31 @@ require "bunny"
 require_relative '../models/user'
 class WorkQueue
   def initialize url
-    @conn = Bunny.new url
-    @conn.start
-    @channel = @conn.create_channel
-    @queue = @channel.queue("user_create")
+    @bunny = Bunny.new url
+    @bunny.start
+    @queue = @bunny.queue("user_create")
   end
 
   def message_count
+    @bunny.start
     @queue.message_count
+    @bunny.stop
   end
 
   def publish_user_create_message(users_as_json)
+    @bunny.start
     @channel.default_exchange.publish(users_as_json, routing_key: @queue.name)
+    @bunny.stop
   end
 
   def start_background
     Thread.new do
       loop do
+        @bunny.start
         @queue.subscribe(block: true) do |_delivery_info, _properties, body|
           User.insert_all JSON.parse(body)
         end
+        @bunny.stop
       end
     end
   end
