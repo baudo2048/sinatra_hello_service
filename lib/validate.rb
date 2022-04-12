9218ef533bb5f2ddd9b444fbaf20bee20a923344
Pito Salas <pitosalas@gmail.com>
2022-04-12 11:00:27 -0400

    wip

--- lib/validate.rb
+++ lib/validate.rb
@@ -1,8 +1,11 @@
 class Validate
   def initialize
+    @logger = Logger.new($stdout)
   end
 
-  def validate_user _user_id
+  def validate_user user_id
+    @logger.info "Pretending to begin validation of user id: #{user_id}"
     sleep 10
+    @logger.info "Pretending to complete validation"
   end
 end
